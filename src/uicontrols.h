9317193918581e81e86f5783445543272f7c28e3
Muhammet Kucuk <mkucuk.1991@msn.com>
2020-05-27 20:30:26 +0300

    Initial commit

---
+++ src/uicontrols.h
@@ -0,0 +1,16 @@
+#ifndef UICONTROLS_H
+#define UICONTROLS_H
+
+#include <QObject>
+
+class UIControls : public QObject
+{
+    Q_OBJECT
+public:
+    explicit UIControls(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // UICONTROLS_H
